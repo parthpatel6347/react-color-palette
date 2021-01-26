0b965495e7d0c35ac173a0527fed158fb16c56fb
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-01-27 01:40:11 +0530

    implement local storage

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import seedColors from "./seedPalette";
 class App extends Component {
   constructor(props) {
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: [...seedColors],
+      palettes: savedPalettes || [...seedColors],
     };
     this.findPalette = this.findPalette.bind(this);
     this.addPalette = this.addPalette.bind(this);
@@ -21,7 +22,17 @@ class App extends Component {
   }
 
   addPalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
